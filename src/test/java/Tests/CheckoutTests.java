7de3c3c1987643381154bd5b9203ca9437fba99a
Stevan Milosevic <bleksteva@gmail.com>
2023-10-19 19:21:50 +0200

    tidying up some things

--- src/test/java/Tests/CheckoutTests.java
+++ src/test/java/Tests/CheckoutTests.java
@@ -6,7 +6,6 @@ import Pages.CheckoutPage;
 import Pages.InventoryPage;
 import Pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
